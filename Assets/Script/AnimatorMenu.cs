72a0837b565e6ffaa3189e5b3fb2799de1a5749e
threepat1 <jtojung@gmail.com>
2020-12-16 03:46:36 +0700

    first

---
+++ Assets/Script/AnimatorMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorMenu : MonoBehaviour
+{
+    public GameObject panel;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ToggleOnOff()
+    {
+
+        if (panel != null)
+        {
+            anim = panel.GetComponent<Animator>();
+            if (anim != null)
+            {
+                bool isOpen = anim.GetBool("Menu");
+
+                anim.SetBool("Menu", !isOpen);
+
+            }
+        }
+
+    }
+}
---
+++ Assets/Script/WaitToPress.cs
@@ -0,0 +1,33 @@
+﻿using OculusSampleFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitToPress : MonoBehaviour
+{
+
+    private InteractableToolsInputRouter interact;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        interact = GetComponent<InteractableToolsInputRouter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DelayClick()
+    {
+        StartCoroutine(WaitASecToPress());
+    }
+
+    public IEnumerator WaitASecToPress()
+    {
+        interact.enabled = false;
+        yield return new WaitForSeconds(2);
+        interact.enabled = true;
+    }
+}
